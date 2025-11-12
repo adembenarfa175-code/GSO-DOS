056413fac8e4cd32d5db97e0b652caae9aecfa2a
adembenarfa175-code <Adembenarfa175@gmail.com>
2025-11-12 10:18:57 +0000

    Cleanup: Removed all remaining legacy MS-DOS batch files, headers (h/), libraries (lib/), and compiler tools.

--- h/copyrigh.h
+++
@@ -1,3 +0,0 @@
-char	*copyright1 =  "GSO-OS Version GSO0 (C)Copyright 1988 GSO Corp";
-char	*copyright2 =  "Licensed Material - Property of GSO-OS";
-
--- h/version.h
+++
@@ -1,6 +0,0 @@
-/*  Use the switch below to produce the standard GSO version 
-    or the GSO version of the operating system */
-
-
-#define GSOCOPYRIGHT 0
-
